000f8be3a8d6ffed2517153facfd810a40406311
DanielleMassey <danimassey1998@gmail.com>
2023-05-16 11:47:47 +0100

    Setup of project

---
+++ TraineeTracker/TraineeTracker.App/Data/TraineeTrackerContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using TraineeTracker.App.Models;
+
+namespace TraineeTracker.App.Data
+{
+    public class TraineeTrackerContext : IdentityDbContext
+    {
+        public TraineeTrackerContext(DbContextOptions<TraineeTrackerContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Tracker> TrackerItems { get; set; }
+
+        public DbSet<Spartan> Spartans { get; set; }
+    }
+}
---
+++ TraineeTracker/TraineeTracker.App/Models/Tracker.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TraineeTracker.App.Models
+{
+    public class Tracker
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Title is required")]
+        public string Title { get; set; } = null!;
+
+        [StringLength(500)]
+        public string? StartDoingText { get; set; }
+
+        [StringLength(500)]
+        public string? StopDoingText { get; set; }
+
+        [StringLength(500)]
+        public string? ContinueDoingText { get; set; }
+
+        [DisplayName("Reviewed")]
+        public bool IsReviewed { get; set; }
+
+        [Range(1, 5)]
+        public int TechnicalSkill { get; set; }
+
+        [Range(1, 5)]
+        public int SpartanSkill { get; set; }
+
+        [DisplayName("Date")]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public Spartan? Spartan { get; set; }
+
+        [ValidateNever]
+        [ForeignKey("Spartan")]
+        public string SpartanId { get; set; } = null!;
+
+        
+    }
+}
