af16f8c366eebaa96a841fe56f4686aecb81f7ac
DanielleMassey <danimassey1998@gmail.com>
2023-05-17 09:42:24 +0100

    experimenting with admin

--- TraineeTracker/TraineeTracker.App/Controllers/HomeController.cs
+++ TraineeTracker/TraineeTracker.App/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using TraineeTracker.App.Data;
 using TraineeTracker.App.Models;
 
 namespace TraineeTracker.App.Controllers
@@ -7,6 +9,7 @@ namespace TraineeTracker.App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TraineeTrackerContext _context;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,6 +24,12 @@ namespace TraineeTracker.App.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Admin()
+        {
+            var spartans = await _context.Spartans.ToListAsync();
+            return View(spartans);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
